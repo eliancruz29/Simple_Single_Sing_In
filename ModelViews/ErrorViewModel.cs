02db2a2d31d49971863fac30224eba124282573e
Elian cruz <eliancruz@Elian-MacBook.local>
2018-11-07 23:54:53 -0400

    Adding the default view for home and error pages

---
+++ ModelViews/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using IdentityServer4.Models;
+
+namespace Simple_Single_Sing_On.ModelViews
+{
+    public class ErrorViewModel
+    {
+        public ErrorMessage Error { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,28 +6,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Simple_Single_Sing_On.Data;
+using Simple_Single_Sing_On.Models;
 
 namespace Simple_Single_Sing_On
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
-        public IConfiguration Configuration { get; }
-        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             string connectionString = Configuration.GetConnectionString("DefaultSecurity");
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+            {
+                config.SignIn.RequireConfirmedEmail = true;
+                config.User.RequireUniqueEmail = true;
+                config.User.AllowedUserNameCharacters = config.User.AllowedUserNameCharacters + " ";
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
 
             // configure identity server with in-memory stores, keys, clients and resources
             var builder = services.AddIdentityServer(options =>
@@ -37,7 +55,7 @@ namespace Simple_Single_Sing_On
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
             })
-                .AddDeveloperSigningCredential()
+                .AddAspNetIdentity<ApplicationUser>()
                 // this adds the config data from DB (clients, resources)
                 .AddConfigurationStore(options =>
                 {
@@ -57,6 +75,15 @@ namespace Simple_Single_Sing_On
                     options.TokenCleanupInterval = 200;
                 });
 
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -69,9 +96,22 @@ namespace Simple_Single_Sing_On
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            // Authenticate before the user accesses secure resources.
             app.UseIdentityServer();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
